0b938b21138d970f96b171c24876ac95c6bc0ac1
arvmarandi <arv.marandi@gmail.com>
2023-07-30 19:04:10 -0700

    dynamic textbox

--- Loreganizer/Loreganizer/MainWindow.xaml.cs
+++ Loreganizer/Loreganizer/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace Loreganizer
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new TextBox
+            TextBox newTextBox = new TextBox();
+
+            // Add any properties you want to set for the TextBox here
+            newTextBox.Width = 150;
+            newTextBox.Margin = new Thickness(5);
+
+            // Set the alignment to center (optional)
+            newTextBox.HorizontalAlignment = HorizontalAlignment.Center;
+            newTextBox.VerticalAlignment = VerticalAlignment.Center;
+
+            // Add the TextBox to the contentGrid
+            contentGrid.Children.Add(newTextBox);
+
+            // Optionally, you can set focus on the new TextBox
+            newTextBox.Focus();
+        }
     }
 }
